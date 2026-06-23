9553ba95a3cd1af8a83f0950e9715da191c25447
Aditya <jamdadeaditya05@gmail.com>
2026-06-23 23:13:06 +0530

    Update CORS for Vercel

--- app.py
+++ app.py
@@ -113,6 +113,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://cred-nova-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
